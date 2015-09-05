afb1a5596a10e5ee4a8df8ac3e1ec94262112503
Anthony Young <ayoung93@gmail.com>
2015-09-05 13:22:15 +0100

    Add configuration

--- express-app/app.js
+++ express-app/app.js
@@ -21,6 +21,11 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+//CONFIG - SECRETS ETC
+var config = require('./config');
+
+//ROUTES
 app.use('/', routes);
 app.use('/users', users);
 
---
+++ express-app/config.js
@@ -0,0 +1,9 @@
+//CONFIG
+module.exports = {
+  port: process.env.port || 3000,
+  twitter: {
+    consumerKey: TWITTER_CONSUMER_KEY,
+    consumerSecret: TWITTER_CONSUMER_SECRET,
+    callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
+  }
+};
